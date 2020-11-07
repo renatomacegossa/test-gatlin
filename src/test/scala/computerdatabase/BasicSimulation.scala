b716579001c28b88b48ca97ec43bf9ed9c929471
renato <renatomacegossa@gmail.com>
2020-11-06 23:26:20 -0300

    commit inicial

---
+++ src/test/scala/computerdatabase/BasicSimulation.scala
@@ -0,0 +1,27 @@
+package computerdatabase
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import scala.concurrent.duration._
+
+class BasicSimulation extends Simulation {
+
+  val httpProtocol = http
+    .baseUrl("http://localhost:8080")
+    .acceptHeader("text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+    .doNotTrackHeader("1")
+    .acceptLanguageHeader("en-US,en;q=0.5")
+    .acceptEncodingHeader("gzip, deflate")
+    .userAgentHeader("Mozilla/5.0 (Windows NT 5.1; rv:31.0) Gecko/20100101 Firefox/31.0")
+
+  val scn = scenario("BasicSimulation")
+    .exec(http("request_1")
+      .get("/registra-evento")
+        .check(status.is(200)))
+    .pause(5)
+
+  setUp(
+//    scn.inject(atOnceUsers(50))
+    scn.inject(constantUsersPerSec(100) during( 60 seconds ))
+  ).protocols(httpProtocol)
+}
